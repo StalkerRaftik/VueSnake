956caa40f2a06f8e42cb8e1d6006a7fae0070331
WhileTrueDoEnd <altair-ivan@ya.ru>
2021-09-21 20:22:33 +1000

    nothing intresting

--- deploy.sh
+++ deploy.sh
@@ -16,10 +16,7 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:stalkerraftik/stalkerraftik.github.io.git main
+git push -f git@github.com:stalkerraftik/VueSnake.git main:gh-pages
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
 
 cd -
